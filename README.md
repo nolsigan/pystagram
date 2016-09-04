b4db7c1aa18738d43029310bfb3b504cb5fb23df
nolsigan <hiayuna@snu.ac.kr>
2016-09-04 19:27:22 +0900

    crawling blogs, update pushed_at

---
+++ README.md
@@ -0,0 +1,16 @@
+# gitblog.io
+
+## Install
+
+### django
+
+* ./manage.py migrate
+* ./manage.py runserver
+
+### redis
+
+* src/redis-server ( in redis folder )
+
+### celery
+
+* celery -A pystagram worker --loglevel=info (debug mode)
--- blog/tasks.py
+++ blog/tasks.py
@@ -4,6 +4,7 @@ from .models import Blog
 import urllib3
 import certifi
 import json
+import pytz
 from datetime import datetime
 
 
@@ -23,16 +24,16 @@ def crawl_blogs():
             ca_certs=certifi.where()
         )
         api_url = blog.get_api_url()
-        print(api_url[1])
+        print('crawling : ' + api_url[1])
         if api_url[0]:
             r = http.request('GET', api_url[1], headers={'User-Agent': 'Mozilla/5.0'})
             obj = json.loads(r.data.decode('utf-8'))
             dt = datetime.strptime(obj['pushed_at'], '%Y-%m-%dT%H:%M:%SZ')
             if dt > blog.pushed_at.replace(tzinfo=None):
                 print('Have to update pushed_at!')
+                blog.pushed_at = dt.replace(tzinfo=pytz.UTC)
+                blog.save()
             else:
                 print('Do not need update')
-
-        # TODO : sending email, update blog.pushed_at
 
     return 'crawl_blogs has blogs "%d" ' % blogs.__len__()
