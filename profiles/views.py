dca99bc9158f0cafe7d2750049110a1148338749
nolsigan <hiayuna@snu.ac.kr>
2016-09-07 12:05:55 +0900

    my page

--- profiles/views.py
+++ profiles/views.py
@@ -6,7 +6,12 @@ from django.contrib.auth import get_user_model
 def profile(request, username):
     users = get_user_model()
     user = get_object_or_404(users, username=username)
+    uid = -1
+    if user.email != '' or user.first_name != '':
+        uid = user.social_auth.get(provider='github').uid
 
     return render(request, 'profile.html', {
         'current_user': user,
+        'uid': uid,
+        'subscribes': user.blog_set.all().count,
     })
